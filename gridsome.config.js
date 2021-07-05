d8292be6b04b391d8ad93c869f0ed3ef7964a67b
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-07-05 15:29:47 -0400

    add ghost source

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,5 +11,18 @@ module.exports = {
       use: "gridsome-plugin-tailwindcss",
       options: {}
     },
-  ]
+    {
+      use: '@gridsome/source-ghost',
+      options: {
+        typeName: 'Ghost',
+        baseUrl: 'http://localhost:2368',
+        contentKey: '0b7050113fba7147f358cc2f4d',
+        version: 'v4' // default
+      }
+    }
+  ],
+  templates: {
+    GhostPost: '/:year/:month/:day/:slug',
+    GhostTag: '/tag/:slug'
+  }
 }
